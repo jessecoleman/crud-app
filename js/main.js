2afca9fd94915af56103fa5f54e9dc37e3543bc2
Jesse C. Chamberlin <jessec18@ougl-18-k.eplt.washington.edu>
2015-10-21 12:17:54 -0700

    initial progress on parse api

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 (function() {
 	window.onload = function() {
+		//initialize parse app
+		Parse.initialize('67M2CjbYLXlKPZwcKbHWkK1m6Gk1rRpVXucDjOIy', 
+						 'pdoBbMrpYabDx8vl8JYzeeBe2VsMl6WFc9Covw5F');
+		//create subclass of Parse.Object
+		var Review = Parse.Object.extend('Music');
 
+		buildRaty():
+
+		$('#review').submit(submitReview);
+	};
+
+	var buildRaty = function() {
+		$('#raty-container').raty({score:5});
+	};
+
+	//handles data sent to Parse.com
+	var submitReview = function() {
+		$(this).find('input').each(function() {
+			Review.set($(this).attr('id'), $(this).val());
+		});
+
+		// after setting properties, save new instance back to database	
+		Review.save();
 	};
 	
 })();
